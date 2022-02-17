5439544c397276bd41f6b41672be62c17b00c73b
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2022-02-16 22:47:46 -0700

    Mark Initial upload

---
+++ greed/game/movingobject.py
@@ -0,0 +1,21 @@
+class MovingObject:
+    """
+    MovingObject class - all moving objects on the screen inherit from this class
+    """
+    def __init__(self, x, y):
+        # Set initial Variables
+        self.center = Point(x,y)
+        self.velocity = Velocity()
+        self.alive = True
+
+
+    def advance(self):
+        # Advance the moving object
+        # If it goes beyond the screen, wrap around to the other side.
+        
+        # x component
+        self.center.x += self.velocity.dx
+        
+        # y component
+        self.center.y += self.velocity.dy
+
---
+++ greed/game/velocity.py
@@ -0,0 +1,11 @@
+class Velocity:
+    """
+    Velocity Class - handles moving object movement
+    """
+    def __init__(self):
+        """
+        Sets up initial values for speed(dx) and direction(dy) of the moving object
+        """
+        self.dx = 1.0  # Starting speed of 1
+        self.dy = 1.0  # Starting speed of 1
+       
