d250bd938aad2c55e531d8a69cd057a792ed40a3
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2022-02-16 22:50:51 -0700

    Mark Initial Upload

---
+++ greed/__main__.py
@@ -0,0 +1,5 @@
+from game.director import Director
+
+# This main program executes the program by instantiating a Director and executing the play_game method.
+director = Director()
+director.play_game()
