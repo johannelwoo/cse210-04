9ebd4089a2fd40ea8cdfbef580e15f7251cb6257
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2022-02-17 17:33:34 -0700

    Mark V2

--- greed/game/shared/velocity.py
+++ greed/game/shared/velocity.py
@@ -6,6 +6,6 @@ class Velocity:
         """
         Sets up initial values for speed(dx) and direction(dy) of the moving object
         """
-        self.dx = 1.0  # Starting speed of 1
-        self.dy = 1.0  # Starting speed of 1
+        self.dx = 0.0  # Starting speed of 0
+        self.dy = -1.0  # Starting speed of -1 (falling)
        
